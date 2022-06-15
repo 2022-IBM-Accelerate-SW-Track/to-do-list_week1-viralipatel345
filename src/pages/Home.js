c813ca80e8080582521bc73862cbf1134315d672
Virali Patel <patelvuh@gmail.com>
2022-06-14 22:05:15 -0400

    Added Todos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-  
+  import "../component/AddTodo.js"; 
+  import "../component/todos.js";
+  import "./Home.css"; 
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      // create your empty list here call it todos.
+       todos : [] // create your empty list here call it todos.
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -28,7 +30,9 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        <p> Replace this</p>
+        <p><Todos todos={this.todo } />
+        <AddTodo addTodo={this.addTodo()} />
+        </p>
       </div>
     );
   }
